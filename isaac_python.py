93db6f64e020aa8e730f42f84adf206e2123f422
Grant Metts <gmetts@andrew.cmu.edu>
2024-02-29 10:35:00 -0800

    Working for Windows

--- isaac_python.py
+++ isaac_python.py
@@ -5,6 +5,40 @@ import json
 import subprocess
 import argparse
 
+def run_windows_system(args):
+    python_ver = platform.python_version_tuple()
+
+    if int(python_ver[1]) != 11:
+        print('Not yet tested for other than python3.11 on Windows, may still work')
+    if os.path.isfile("config.json"):
+        print('found json')
+        f = open('config.json')
+        configs = json.load(f)
+        isaac_path = configs['isaac_path']
+    else:
+        print(os.getcwd()+"/config.json not found, searching default isaac install path")
+
+        isaac_path = os.path.expanduser('~')+'\AppData\Local\ov\pkg'
+        if not os.path.isdir(isaac_path):
+            print('Default install path not found')
+            exit()
+        dir_list = os.listdir(isaac_path)
+        isaac_list = [s for s in dir_list if "isaac" in s]
+        if len(isaac_list) == 0:
+            print('No Isaac install found in default path. Omniverse likely installed but no Isaac installed')
+        elif len(isaac_list) > 1:
+            print('Multiple Isaac versions found, using newest version ' + isaac_list[0])
+        
+        isaac_path = isaac_path+"/"+isaac_list[0]
+
+    if not "2023" in isaac_path:
+        print("Warning, this has only been tested for Isaac 2023")
+
+    file_path = os.getcwd() + "/" + args.file_path
+    print(os.path.isfile(isaac_path + "/python.bat"))
+    subprocess.run([isaac_path+"/python.bat", file_path])
+    exit()
+
 def run_linux_system(args):
     python_ver = platform.python_version_tuple()
     if int(python_ver[1]) > 8:
@@ -58,6 +92,8 @@ if __name__ == "__main__":
     system = platform.system()
     if system == 'Linux':
         run_linux_system(args)
+    elif system == 'Windows':
+        run_windows_system(args)
     else:
         print("System not yet supported")
         exit()
