63ea16f30555f8739227ed978ed8a9a54772e6fb
Ryan Wu <weihuanw@andrew.cmu.edu>
2024-04-17 19:16:37 -0400

    added comments

--- src/TheiaSLAM/main.py
+++ src/TheiaSLAM/main.py
@@ -3,10 +3,10 @@
 from omni.isaac.kit import SimulationApp
 
 # run with Isaac Sim visualization
-# CONFIG = {"width": 1280, "height": 720, "sync_loads": True, "headless": False, "renderer": "RayTracedLighting"}
+CONFIG = {"width": 1280, "height": 720, "sync_loads": True, "headless": False, "renderer": "RayTracedLighting"}
 
 # run without Isaac Sim visualization
-CONFIG = {"sync_loads": True, "headless": True}
+# CONFIG = {"sync_loads": True, "headless": True}
 
 #TODO: Create config json
 kit = SimulationApp(launch_config=CONFIG)
